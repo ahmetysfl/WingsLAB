f90d1da59f207e1bfd8e3acf5de2c8aee3c8506e
ahmetysfl <ahm.ysfl@gmail.com>
2017-04-15 23:00:24 +0300

    AE

--- liquidExample/main.c
+++ liquidExample/main.c
@@ -1,59 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include <complex.h>
 #include <liquid/liquid.h>
 
-// user-defined static callback function
-static int mycallback(unsigned char *  _header,
-                      int              _header_valid,
-                      unsigned char *  _payload,
-                      unsigned int     _payload_len,
-                      int              _payload_valid,
-                      framesyncstats_s _stats,
-                      void *           _userdata)
+int cnv2digital(float f)
 {
-    printf("***** callback invoked!\n");
-    printf("    header  (%s)\n",  _header_valid ? "valid" : "INVALID");
-    printf("    payload (%s)\n", _payload_valid ? "valid" : "INVALID");
-    framesyncstats_print(&_stats);
+    int i = f * 2048;
 
-    for(int i=0;i<_payload_len;i++)
+    if (i>2047)
     {
-
+        i=2047;
     }
-
-    // type-cast, de-reference, and increment frame counter
-    unsigned int * counter = (unsigned int *) _userdata;
-    (*counter)++;
-
-    return 0;
+    else if(i<-2048)
+    {
+        i=-2048;
+    }
 }
-
 int main() {
-    FILE *fp;
-    fp = fopen( "file.txt" , "w" );
-
-    // options
-    unsigned int frame_counter   =   0;     // userdata passed to callback
-    float        phase_offset    =   0.3f;  // carrier phase offset
-    float        frequency_offset=   0.02f; // carrier frequency offset
-    float        SNRdB           =  10.0f;  // signal-to-noise ratio [dB]
-    float        noise_floor     = -40.0f;  // noise floor [dB]
-
     // allocate memory for arrays
     unsigned char header[8];                    // data header
     unsigned char payload[64];                  // data payload
     unsigned int  buf_len = LIQUID_FRAME64_LEN; // length of frame
     float complex buf[buf_len];                 // sample buffer
 
-    // create frame generator
+    // CREATE frame generator
     framegen64 fg = framegen64_create();
     framegen64_print(fg);
-
-    // create frame synchronizer using default properties
-    framesync64 fs = framesync64_create(mycallback, (void*)&frame_counter);
-    framesync64_print(fs);
 
     // initialize header, payload
     unsigned int i;
@@ -62,26 +36,27 @@ int main() {
     for (i=0; i<64; i++)
         payload[i] = rand() & 0xff;
 
-    // EXECUTE generator and assemble the frame, storing samples in buffer
+    // EXECUTE generator and assemble the frame
     framegen64_execute(fg, header, payload, buf);
 
-    // add channel impairments (attenuation, carrier offset, noise)
-    float nstd  = powf(10.0f, noise_floor/20.0f);        // noise std. dev.
-    float gamma = powf(10.0f, (SNRdB+noise_floor)/20.0f);// channel gain
-    for (i=0; i<buf_len; i++) {
-        buf[i] *= gamma;
-        buf[i] *= cexpf(_Complex_I*(phase_offset + i*frequency_offset));
-        buf[i] += nstd * (randnf() + _Complex_I*randnf())*M_SQRT1_2;
+    FILE * pFile;
+    char stringLine[16];
+    pFile = fopen ("tx.csv", "wb");
+    // print a few of the generated frame samples to the screen
+    for (i=0; i<buf_len; i++)
+    {
+        float real_part = crealf(buf[i]);
+        float img_part = cimagf(buf[i]);
+
+        fprintf (pFile , "%d,%d\n",cnv2digital(real_part), cnv2digital(img_part));
+        printf("%3u : %12.8f + j*%12.8f\n", i, real_part, img_part);
+        printf("___ : %d + j*%d\n\n", cnv2digital(real_part), cnv2digital(img_part));
     }
 
-    // EXECUTE synchronizer and receive the frame in one block
-    framesync64_execute(fs, buf, buf_len);
-
+    fclose(pFile);
     // DESTROY objects
     framegen64_destroy(fg);
-    framesync64_destroy(fs);
 
-    printf("received %u frames\n", frame_counter);
     printf("done.\n");
     return 0;
 }
