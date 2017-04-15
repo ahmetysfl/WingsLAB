b0c980a9d7b3cb085b843a656facf432b48ede3b
ahmetysfl <ahm.ysfl@gmail.com>
2017-04-15 16:40:18 +0300

    AE

--- exampleRF/main.c
+++ exampleRF/main.c
@@ -80,28 +80,67 @@ int configure_module(struct bladerf *dev, struct module_config *c)
     return status;
 }
 
-int main()
+/* Usage:
+ *   libbladeRF_example_boilerplate [serial #]
+ *
+ * If a serial number is supplied, the program will attempt to open the
+ * device with the provided serial number.
+ *
+ * Otherwise, the first available device will be used.
+ */
+int main(int argc, char *argv[])
 {
     int status;
+    struct module_config config;
     struct bladerf *dev = NULL;
     struct bladerf_devinfo dev_info;
-
     /* Initialize the information used to identify the desired device
      * to all wildcard (i.e., "any device") values */
     bladerf_init_devinfo(&dev_info);
-
-    strcpy(dev_info.serial,"62fd1f9210e0940a2c22f705056305cb");
-    printf("Serial: %s !\n",dev_info.serial);
-    printf("USB BUS: %d !\n",dev_info.usb_bus);
-    printf("USB ADDR: %d !\n",dev_info.usb_addr);
-    printf("Instance: %d !\n",dev_info.instance);
-
+    /* Request a device with the provided serial number.
+     * Invalid strings should simply fail to match a device. */
+    if (argc >= 2) {
+        strncpy(dev_info.serial, argv[1], sizeof(dev_info.serial) - 1);
+    }
     status = bladerf_open_with_devinfo(&dev, &dev_info);
     if (status != 0) {
-            fprintf(stderr, "Unable to open device: %s\n",
-                    bladerf_strerror(status));
-            return 1;
-        }
-    return 0;
+        fprintf(stderr, "Unable to open device: %s\n",
+                bladerf_strerror(status));
+        return 1;
+    }
+    /* Set up RX module parameters */
+    config.module     = BLADERF_MODULE_RX;
+    config.frequency  = 910000000;
+    config.bandwidth  = 2000000;
+    config.samplerate = 300000;
+    config.rx_lna     = BLADERF_LNA_GAIN_MAX;
+    config.vga1       = 30;
+    config.vga2       = 3;
+    status = configure_module(dev, &config);
+    if (status != 0) {
+        fprintf(stderr, "Failed to configure RX module. Exiting.\n");
+        goto out;
+    }
+    /* Set up TX module parameters */
+    config.module     = BLADERF_MODULE_TX;
+    config.frequency  = 918000000;
+    config.bandwidth  = 1500000;
+    config.samplerate = 250000;
+    config.vga1       = -14;
+    config.vga2       = 0;
+    status = configure_module(dev, &config);
+    if (status != 0) {
+        fprintf(stderr, "Failed to configure TX module. Exiting.\n");
+        goto out;
+    }
+    /* Application code goes here.
+     *
+     * Don't forget to call bladerf_enable_module() before attempting to
+     * transmit or receive samples!
+     */
+out:
+    bladerf_close(dev);
+    return status;
 }
+
 
