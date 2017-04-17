e816063cad308dc8cb68c6a4976e8d660f7e06c2
ahmetysfl <ahm.ysfl@gmail.com>
2017-04-17 23:38:17 +0300

    AE

--- exampleRF/main.c
+++ exampleRF/main.c
@@ -290,6 +290,8 @@ int main(int argc, char *argv[])
     meta.flags = 0;
     /* Retrieve the current timestamp */
     status = bladerf_get_timestamp(dev, BLADERF_MODULE_RX, &meta.timestamp);
+
+    meta.timestamp = meta.timestamp + 1000000;
     if (status != 0) {
         fprintf(stderr, "Failed to get current RX timestamp: %s\n",
                 bladerf_strerror(status));
